c4d3e3d87de93c5bfc3b7f8890ce076fabde8a68
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-02-15 00:07:43 -0500

    Complete first draft of data prep.

--- dataprep/dataprep.py
+++ dataprep/dataprep.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
 from __future__ import print_function
+from operator import itemgetter
 import urllib2
 from bs4 import BeautifulSoup
 import skimage
 import skimage.io
 import skimage.draw
+import skimage.transform
 import re
+import numpy
 
 
 class Debug:
@@ -20,7 +23,7 @@ class GroundTruthError(Exception):
 		self.message = message
 
 class Face:
-	def __init__(self):
+	def __init__(self, expansion=0.15):
 		self.left_eye = ()
 		self.right_eye = ()
 		self.left_mouth = ()
@@ -28,34 +31,37 @@ class Face:
 		self.center_mouth = ()
 		self.nose = ()
 		self.__square = None
+		self.expansion = expansion
+
+	def __repr__(self):
+		return str(self.square())
 
 	def square(self):
 		if self.__square == None:
-			# Find smallest y
-			if self.left_eye[0] < self.right_eye[0]:
-				top = self.left_eye[0]
-			else:
-				top = self.right_eye[0]
-			# Find smallest x
-			if self.left_eye[1] < self.left_mouth[1]:
-				left = self.left_eye[1]
-			else:
-				left = self.left_mouth[1]
-			# Find biggest x
-			if self.left_mouth[1] > self.right_mouth[1]:
-				right = self.left_mouth[1]
-			else:
-				right = self.right_mouth[1]
-			# Find biggest y
-			if self.left_mouth[0] > self.right_mouth[0]:
-				bottom = self.left_mouth[0]
-			else:
-				bottom = self.right_mouth[0]
-			self.__square = (top, left, bottom, right)
+			# Well, nuts. The faces could be upside down.
+			# We have to do this the hard way
+			features = [self.left_eye, self.right_eye, self.left_mouth, self.nose, self.center_mouth, self.right_mouth];
+
+			features = sorted(features, key=itemgetter(0))
+			top = features[0][0]
+			features = sorted(features, key=itemgetter(0), reverse=True)
+			bottom = features[0][0]
+			features = sorted(features, key=itemgetter(1))
+			left = features[0][1]
+			features = sorted(features, key=itemgetter(1), reverse=True)
+			right = features[0][1]
+
+			height_expansion = int(((bottom - top) * self.expansion) / 2.0)
+			width_expansion = int(((right - left) * self.expansion) / 2.0)
+			self.__square = (top - height_expansion,
+				left - width_expansion,
+				bottom + height_expansion,
+				right + width_expansion)
 		return self.__square
 
 class GroundTruth:
 	def __init__(self, url=None, filename=None):
+		self.face_count = 0
 		self.faces = {}
 		if (url != None and filename != None) or\
 		   (url == None and filename == None):
@@ -93,13 +99,13 @@ class GroundTruth:
 					parsed_line = self._parse_line(l)
 					Debug.Print("line: " + str(l))
 					Debug.Print("parsed line: " + str(parsed_line))
+					self.face_count += 1
 					if self.faces.has_key(dataset + "/" + parsed_line[0]):
 						self.faces[dataset + "/" + parsed_line[0]].append(parsed_line)
 					else:
 						self.faces[dataset + "/" + parsed_line[0]] = [parsed_line]
 
 			e = e.findNext(['h1','pre'])
-		print("Done")
 
 	def _parse_line(self, line):
 		ls = line.split(" ")
@@ -119,19 +125,37 @@ class GroundTruth:
 			for filename_face in self.faces[filename]:
 				filename = filename_face[0]
 				face = filename_face[1]
-				print("filename: " + filename)
-				print("face: " + str(face))
-				rr, cc = skimage.draw.line(*(face.square()))
+				Debug.Print("filename: " + filename)
+				Debug.Print("face: " + str(face))
+
+				top, left, bottom, right = face.square()
+				rr, cc = skimage.draw.line(top, left, top, right)
 				image[rr,cc] = 1.0
-				rr, cc = skimage.draw.line(*(face.left_eye + face.right_eye))
+				rr, cc = skimage.draw.line(top, right, bottom, right)
 				image[rr,cc] = 1.0
-				rr, cc = skimage.draw.line(*(face.left_eye + face.left_mouth))
+				rr, cc = skimage.draw.line(bottom, right, bottom, left)
 				image[rr,cc] = 1.0
-				rr, cc = skimage.draw.line(*(face.right_eye + face.right_mouth))
-				image[rr,cc] = 1.0
-				rr, cc = skimage.draw.line(*(face.left_mouth + face.right_mouth))
+				rr, cc = skimage.draw.line(bottom, left, top, left)
 				image[rr,cc] = 1.0
 			skimage.io.imsave(output_dir + "/" + filename, image)
+
+	def extract_face_patches(self):
+		patches = []
+		for filename in self.faces.keys():
+			image = skimage.img_as_float(skimage.io.imread("./data/" + filename))
+			counter = 0
+			for filename_face in self.faces[filename]:
+				filename = filename_face[0]
+				face = filename_face[1]
+
+				Debug.Print("filename: " + filename)
+				Debug.Print("face: " + str(face))
+				Debug.Print("counter: " + str(counter))
+
+				top, left, bottom, right = face.square()
+				patches.append((filename, counter, image[top:bottom, left:right]))
+				counter+=1
+		return patches
 
 	def _download_ground_truth(self,url):
 		req = urllib2.Request(url=url)
@@ -148,6 +172,24 @@ class GroundTruth:
 			return ""
 		return fh.read()
 
+def generate_face_mosaic(patches, mosaic_filename, height=12, width=12):
+	counter = 0
+	mosaic_width = 12
+	mosaic = numpy.zeros(((height*(1+(len(patches)/mosaic_width))),
+		width*mosaic_width))
+	for (filename, _, image) in patches:
+		y_spot = counter/mosaic_width
+		x_spot = counter%mosaic_width
+		print("y_spot: %d x_spot: %d" % (y_spot, x_spot))
+		scaled = skimage.transform.resize(image, (height,width))
+		mosaic[(y_spot*height):((y_spot*height)+height),\
+		       (x_spot*width):((x_spot*width)+width)] =\
+			scaled[0:height,0:width]
+		counter += 1
+	skimage.io.imsave(mosaic_filename, mosaic)
+
 if __name__ == "__main__":
 	d = GroundTruth(filename="./data.html")
-	d.draw_face_squares("./output")
+	#d.draw_face_squares("./output")
+	patches = d.extract_face_patches()
+	generate_face_mosaic(patches, "./output/mosaic.gif")
