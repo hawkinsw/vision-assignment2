93b854da5d3d6f2fed3ba5907d5d95f5570d9d88
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-02-19 21:43:11 -0500

    Clean up gaussian a little.

--- gaussian.py
+++ gaussian.py
@@ -18,7 +18,13 @@ def sigma(shape, mu, arrays):
 	for a in arrays:
 		A[row] = numpy.transpose(a-mu)
 		row += 1
-	return numpy.dot(A.T,A)/(1.0*rows)
+	result = A.T.dot(A)/(1.0*rows)
+	"""
+	print("A.T.shape: %s" % str(A.T.shape))
+	print("A.shape: %s" % str(A.shape))
+	print("result.shape: %s" % str(result.shape))
+	"""
+	return result
 
 def FitGaussian(sample_shape, samples):
 	_mu = mu(sample_shape, samples)
@@ -29,6 +35,14 @@ class Gaussian:
 	def __init__(self, mu, sigma, tau=1.0e-0):
 		self.mu = mu
 		(u,s,u_t) = numpy.linalg.svd(sigma)
+
+		"""
+		print("u.shape: %s" % str(u.shape))
+		print("s.shape: %s" % str(numpy.diag(s).shape))
+		print("u_t.shape: %s" % str(u_t.shape))
+		print("s: " + str(s))
+
+		"""
 		#
 		# Find out how much of sigma is larger than tau.
 		#
@@ -41,15 +55,24 @@ class Gaussian:
 		trunc_s = s[0:self.k]
 		trunc_u_t = trunc_u.T
 		inv_trunc_s = 1.0/trunc_s
-		self.sigma = numpy.dot(numpy.dot(trunc_u, numpy.diag(trunc_s)), trunc_u_t)
-		# http://en.wikipedia.org/wiki/Singular_value_decomposition#Applications_of_the_SVD
-		self.inv_sigma = numpy.dot(numpy.dot(trunc_u, numpy.diag(inv_trunc_s)), trunc_u_t)
-		self.det_sigma = reduce(lambda x,y: x*y, trunc_s)
-		print("det_sigma: " + str(self.det_sigma))
 
+		self.sigma = trunc_u.dot(numpy.diag(trunc_s)).dot(trunc_u_t)
+
+		"""
+		print("trunc_u.shape: %s" % str(trunc_u.shape))
+		print("trunc_s.shape: %s" % str(numpy.diag(trunc_s).shape))
+		print("trunc_u_t.shape: %s" % str(trunc_u_t.shape))
+		print("self.sigma.shape: %s" % str(self.sigma.shape))
+		"""
+		# http://en.wikipedia.org/wiki/Singular_value_decomposition#Applications_of_the_SVD
+		self.inv_sigma = trunc_u.dot(numpy.diag(inv_trunc_s)).dot(trunc_u_t)
+		self.det_sigma = numpy.multiply.reduce(trunc_s)
 
 	def evaluate(self, x):
-		denominator = 1.0 /\
-			(numpy.sqrt(math.pow(2.0*numpy.pi, self.k)*self.det_sigma))
-		exponent = -0.5*numpy.dot(numpy.dot(numpy.transpose(x-self.mu),self.inv_sigma),(x-self.mu))
-		return reduce(lambda x,y: x+y, (numpy.exp(exponent)*denominator).ravel().tolist())
+		denominator = (numpy.sqrt(math.pow(2.0*numpy.pi, self.k)*self.det_sigma))
+		exponent=-0.5*numpy.transpose(x-self.mu).dot(self.inv_sigma).dot(x-self.mu)
+		"""
+		print("exponent.shape: %s" % str(exponent.shape))
+		"""
+		exponent = numpy.add.reduce(numpy.add.reduce(exponent))
+		return numpy.exp(exponent)/denominator
