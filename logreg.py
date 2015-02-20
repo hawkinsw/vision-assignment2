d28709f0a03dc6bd9cfd8d21faa436185752f795
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-02-19 20:33:08 -0500

    Use actual convergence calculation and large number of iterations.

--- logreg.py
+++ logreg.py
@@ -7,28 +7,43 @@ import skimage
 import skimage.io
 
 def g(observation, estimate):
-	exponent = -1.0*numpy.dot(estimate.T, observation)
-	return 1.0/(1.0 + numpy.exp(numpy.add.reduce(numpy.add.reduce(exponent))))
+	#
+	# Use ravel() here to make these vectors
+	# instead of column/row matrices.
+	#
+	exponent = -1.0*(numpy.dot(estimate.ravel(),observation.ravel()))
+	return 1.0/(1.0 + numpy.exp(exponent))
 
 def FitLogReg(observations, classifications, mu):
 	assert len(observations) == len(classifications),\
 		"Number of observations and classifications must match."
+
+	max_iterations = 200
+	convergence = 1.0e-2
 	_w = numpy.zeros((observations[0].shape[0]+1,1))
 
+	#
+	# "Canonicalize" the data representations.
+	#
 	for i in range(len(observations)):
-		print("obs shape: %s" % str(observations[i].shape))
 		observations[i] = numpy.vstack([observations[i], [1]])
-		print("obs shape: %s" % str(observations[i].shape))
 
 	iterations = 0
 	while True:
 		summation = numpy.zeros(_w.shape)
 		for o,c in zip(observations, classifications):
 			summation += (c - g(o,_w))*o
-		_w = _w + mu*summation
-		iterations += 1
-		if iterations > 100:
+
+		scaled_summation = mu*summation
+		_w_delta_sum = numpy.add.reduce(scaled_summation)
+		_w = _w + scaled_summation
+
+		if math.fabs(_w_delta_sum) <= convergence:
 			break
+		iterations += 1
+		if iterations > max_iterations:
+			break
+
 	return _w
 
 class LogReg:
