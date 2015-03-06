b91eb450ef888230821d276ea830c61dd3057ffd
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-03-05 20:25:52 -0500

    Update format of output.html

--- make_output.sh
+++ make_output.sh
@@ -2,13 +2,14 @@
 
 rm -f output.html
 echo "<table>" >> output.html
-echo "<tr><td>&nbsp;</td><td>Original</td><td colspan=2>Gaussian</td><td colspan=2>Linear</td></tr>" >> output.html
-echo "<tr><td>&nbsp;</td><td>&nbsp;</td><td><b>No</b> Non Max Suppression</td><td>Non Max Suppression</td><td><b>No</b> Non Max Suppression</td><td>Non Max Suppression</td></tr>" >> output.html
-for i in `ls -r test_input/*.jpg | grep -v faces`; do
-	short_i=${i%.jpg}
+echo "<tr><td>&nbsp;</td><td>Description</td><td>Original</td><td colspan=2>Gaussian</td><td colspan=2>Linear</td></tr>" >> output.html
+echo "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td><b>No</b> Non Max Suppression</td><td>Non Max Suppression</td><td><b>No</b> Non Max Suppression</td><td>Non Max Suppression</td></tr>" >> output.html
+for i in `ls -r test_input/*.gif | grep -v faces`; do
+	short_i=${i%.gif}
 	short_i=${short_i#*/}
 	echo "<tr>"
 	echo "<td>${short_i}</td>"
+	echo "<td>&nbsp;</td>"
 	echo "<td><img src=\"$i\"/></td>"
 	for j in `ls -r test_output/*${short_i}_* 2> /dev/null | grep gaussian`; do
 		echo "<td><img src=\"$j\"/></td>"
